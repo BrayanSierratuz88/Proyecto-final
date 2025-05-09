e7da5656c4ffb7bc79322eae17f5b02dde3922ae
BrayanSierratuz88 <sierratuz@gmaol.com>
2025-05-08 22:54:35 -0600

    dia

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -14,16 +14,16 @@ const router = express.Router();
 // Auth Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/profile", protect, getUserProfile);   // GET para obtener datos
-router.put("/update", protect, updateUserProfile); // PUT para actualizar datos
+router.get("/profile", protect, getUserProfile);
+router.put("/update", protect, updateUserProfile);
 
-
-router.post("/uplaod-image" , upload.single("image"), (req, res) => {
+// **RUTA CORREGIDA**
+router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
   const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
   res.status(200).json({ imageUrl });
-}
-);
+});
+
 module.exports = router;
