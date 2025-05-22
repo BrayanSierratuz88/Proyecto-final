fc1f33cd45239d10eaf7142351ab338b07ef2785
BrayanSierratuz88 <sierratuz@gmaol.com>
2025-05-21 23:47:20 -0600

    adelanto

--- frontend/task-manager/src/pages/Auth/Login.jsx
+++ frontend/task-manager/src/pages/Auth/Login.jsx
@@ -1,12 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { Link, useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/input';
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const navigate = useNavigate();
+
+  // handle login form submit
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if(!validateEmail(email)){
+      setError("Please enter a valid Email address.");
+      return;
+    }
+    if(!password){
+      setError("Please enter the password");
+      return;
+    }
+    setError("");
+    //login Api Call
+
+    // Aquí va la lógica de autenticación
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+          />
+            <Input
+            value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Characters"
+             type="password" // <-- minúsculas
+/>
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          <button type= "submit" className="btn-primary">
+            LOGIN
+          </button>
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account? {" "}
+            <Link className="font-medium text-primary urdeline" to="/signup">
+            SignUp
+            </Link>
+            </p>
+          </form>
+      </div>
+    </AuthLayout>
+  );
+};
+
+export default Login;
 
--- frontend/task-manager/src/pages/Auth/SingUp.jsx
+++ frontend/task-manager/src/pages/Auth/SingUp.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
 
 const SingUp = () => {
   return (
-    <div>
-      
-    </div>
+    <AuthLayout>
+      SingUp
+    </AuthLayout>
   )
 }
 
