084345d5ebacf94075e15cef534d8253ed048066
BrayanSierratuz88 <sierratuz@gmaol.com>
2025-05-25 23:36:49 -0600

    Hola

--- frontend/task-manager/src/pages/admin/Dashboard.jsx
+++ frontend/task-manager/src/pages/admin/Dashboard.jsx
@@ -3,7 +3,7 @@ import { useUserAuth } from '../../hooks/useUserAuth';
 import { UserContext } from '../../context/userContext';
 import { useContext } from 'react';
 import DashboardLayout from '../../components/layouts/DashboardLayout';
-import { useNavigate } from 'react-router-dom';
+import { data, useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
 import moment from 'moment'
@@ -29,6 +29,28 @@ const Dashboard = () => {
   const [barChartData,setBarChartData] = useState([]);
 
 
+  //Prepare Chart Data
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      {status : "Pending",count: taskDistribution?.Pending|| 0 },
+      { status : "In Progress", count: taskDistribution?.InProgress|| 0},
+      {status : " Completed", count: taskDistribution?.Completed|| 0},
+    ];
+
+    setPieChartData(taskDistributionData);
+    const PriorityLevelData= [
+      { priority: "Low",count: taskPriorityLevels?.Low || 0},
+      {priority: "Medium",count: taskPriorityLevels?.Medium || 0},
+      {priority: "High",count: taskPriorityLevels?.High || 0},
+    ];
+
+    setBarChartData(PriorityLevelData);
+  };
+
+
   const getDashboardData = async () => {
     try { 
       const response = await axiosInstance.get(
@@ -36,6 +58,7 @@ const Dashboard = () => {
       );
       if(response.data){
         setDashboardData(response.data);
+        prepareChartData(response.data?.charts || null)
       }
     } catch (error) {
       console.error("Error fetching users:", error);
@@ -97,6 +120,10 @@ const Dashboard = () => {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6">
+            
+
+
+
           <div className="md-col-span-2">
             <div className="card">
               <div className="flex items-center justify-between">
