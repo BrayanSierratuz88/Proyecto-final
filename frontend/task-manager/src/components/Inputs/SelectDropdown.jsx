f374ec87c89f1386c7f1bb448aad2aa6814aa40c
BrayanSierratuz88 <sierratuz@gmaol.com>
2025-05-27 20:55:46 -0600

    Avances

---
+++ frontend/task-manager/src/components/Inputs/SelectDropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectDropdown = ({options,value,onChange,placeholder}) => {
+  return (
+    <div>
+      SelectDropdown
+    </div>
+  )
+}
+
+export default SelectDropdown
